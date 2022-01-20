fca84a6635496f82ca76b5e605d356d8799057cf
jang-insu <oasisholics@gmail.com>
2022-01-20 23:08:35 +0900

    유저 스키마&모델 생성 및 register api 생성

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 5000
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+// application/json
+app.use(bodyParser.json());
+
+const {User} = require('./models/User')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://insoo:parameter5507@bolierplate.fkht7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -11,5 +20,14 @@ mongoose.connect('mongodb+srv://insoo:parameter5507@bolierplate.fkht7.mongodb.ne
 
 
 app.get('/', (req, res) => res.send('Hello World'))
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err,userInfo) => {
+        if(err) return res.json({success:false, err}) 
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
