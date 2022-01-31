d79a2aa2f727a3781f9f55e98808f94aa3090651
jang-insu <oasisholics@gmail.com>
2022-01-31 16:00:20 +0900

    add loing/auth/logout functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 5000
 
@@ -10,8 +11,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const {User} = require('./models/User')
+const {auth}= require('./middleware/auth')
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -22,7 +25,7 @@ mongoose.connect(config.mongoURI, {
 
 
 app.get('/', (req, res) => res.send('Hello World,Insoo'))
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
     user.save((err,userInfo) => {
         if(err) return res.json({success:false, err}) 
@@ -31,5 +34,63 @@ app.post('/register', (req, res) => {
         })
     })
 })
+
+app.post('/api/users/login', (req, res)=>{
+    User.findOne({email: req.body.email}, (err, userInfo) => {
+        if(!userInfo){
+            return res.json({
+                loginSuccess:false,
+                message : '일치하는 이메일이 없습니다!'
+            })
+        }
+
+        userInfo.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch){
+                return res.json({
+                    loginSuccess:false,
+                    message: '비밀번호가 틀렸습니다.'
+                })
+            }
+            
+            userInfo.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err)
+
+                res.cookie('x_auth', user.token)
+                .status(200)
+                .json({
+                    loginSuccess : true,
+                    userId : user._id
+                })
+            })
+
+        });
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth : true,
+        email : req.user.email,
+        name : req.user.name,
+        lastname : req.user.lastname,
+        role: req.user.role,
+        image:req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id:req.user._id}, 
+        {
+            token : ""
+        }, (err, user) => {
+            if(err) return res.json({success : false, err});
+            return res.status(200).send(
+                {success : true}
+            )
+        })
+})
+
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const saltRounds = 10; // using salt, encrypt password. rounds means salt length
 
 const userSchema = mongoose.Schema({
     name:{
@@ -30,6 +33,61 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        // encrypt password
+        // 1. generate salt, 2. encrypt password using salt
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                // Store hash in your password DB.
+                user.password = hash
+                next()
+            });
+        });
+    }else{
+        next()
+    }
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback){
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch){
+        if(err) return callback(err);
+
+        callback(err, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(callback){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token 
+    user.save(function(err, user){
+        if(err) return callback(err)
+
+        callback(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, callback){
+    var user = this;
+
+    //decode token 
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        if(err) callback(err)
+
+        user.findOne({"_id" : decoded, "token" : token}, function(err, user){
+            if(err) return callback(err)
+
+            callback(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
